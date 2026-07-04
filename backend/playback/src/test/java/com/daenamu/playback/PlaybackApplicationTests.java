7770960546eb6eed58cfdde1f80ffcadd579d925
HyominAn0401 <slhm7547@naver.com>
2026-07-04 13:11:54 +0900

    initial commit

---
+++ backend/playback/src/test/java/com/daenamu/playback/PlaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daenamu.playback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
